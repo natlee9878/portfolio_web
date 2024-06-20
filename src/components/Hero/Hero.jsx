3aa74cb87a9aef4df004e98a4acc002762ce001f
natlee9878 <natleejianyang@gmail.com>
2024-06-21 02:42:48 +0930

    added dist

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,28 +1,22 @@
-import React from 'react';
-import { getImageUrl } from '../../utils';
+import React from "react";
+import { getImageUrl } from "../../utils";
 import styles from "./Hero.module.css";
 
 export const Hero = () => {
-    return (
-        <section className={styles.container}>
-            <div className={styles.content}>
-                {/*<h4 className={styles.smalltext}>Hi, my name is</h4>*/}
-                <h2 className={styles.title}>Hi, I'm Nathaniel</h2>
-                <p className={styles.subtitle}>I build websites and software.</p>
-                <p className={styles.description}>
-                    Full-Stack Web Developer / Software Developer
-                </p>
-                <a href="mailto:natleejianyang@gmail.com" className={styles.contactBtn}>
-                    Let's talk
-                </a>
-            </div>
-            {/* <img
-                src={getImageUrl("hero/circlenat.png")}
-                alt="Hero image of me"
-                className={styles.heroImg}
-            /> */}
-            <div className={styles.topBlur} />
-            {/* <div className={styles.bottomBlur} /> */}
-        </section>
-    );
+  return (
+    <div className={styles.heroWrapper}>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Hi, I'm Nathaniel</h2>
+        <p className={styles.subtitle}>I build websites and software.</p>
+        <p className={styles.description}>
+          Full-Stack Web Developer / Software Developer
+        </p>
+        <a href="mailto:natleejianyang@gmail.com" className={styles.contactBtn}>
+          Let's talk
+        </a>
+      </div>
+    </section>
+    </div>
+  );
 };
